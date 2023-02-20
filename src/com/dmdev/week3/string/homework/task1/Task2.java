bf0f6e6d7334964713c917f8120d11b1fd7ff0d0
aleksandrbelaev <alexanderbelyaev1990@gmail.com>
2023-02-20 18:50:41 +0200

    решение Task2 решение задачи про строку с цифрами

--- src/com/dmdev/week3/string/homework/task1/Task2.java
+++ src/com/dmdev/week3/string/homework/task1/Task2.java
@@ -20,46 +20,42 @@ import java.util.Arrays;
 public class Task2 {
     public static void main(String[] args) {
         String value = "Привет 8, как 1 2 твои дела? Может4, сделать 3 дело?";
-        arrayInt(value);
-        sumArrayElements(value);
+        System.out.println(value);
+        int[] result = arrayInt(value);
+        int sumResult = sumArrayElements(value);
+        System.out.println(Arrays.toString(result));
+        System.out.println(sumResult);
     }
 
     private static int[] arrayInt(String value) {
-        int[] array = new int[1];
+
         char[] chars = value.toCharArray();
         StringBuilder str = new StringBuilder();
-        char a0 = '0';
-        char a1 = '1';
-        char a2 = '2';
-        char a3 = '3';
-        char a4 = '4';
-        char a5 = '5';
-        char a6 = '6';
-        char a7 = '7';
-        char a8 = '8';
-        char a9 = '9';
-        for (int i = 0; i < chars.length; i++) {
-            System.out.print(chars[i]);
-            if (chars[i] == a0 || chars[i] == a1 ||chars[i] == a2 ||chars[i] == a3 ||chars[i] == a4 ||chars[i] == a5 ||chars[i] == a6 ||chars[i] == a7 ||chars[i] == a8 || chars[i] == a9) {
-                str.append(chars[i]);
+        int count = 0;
+        for (char aChar : chars) {
+
+            if (Character.isDigit(aChar)) {
+                str.append(aChar);
+                count++;
             }
         }
-        char[] charsResult = str.toString().toCharArray();
-        System.out.println();
-        System.out.println(charsResult);
-        int[] result = new int[charsResult.length];
-        for (int i = 0; i < charsResult.length; i++) {
-            result[i] = charsResult[i];
-            System.out.println(charsResult[i]);
-            System.out.println();
-            System.out.println(result[i]);
+
+        int[] array = new int[count];
+        char[] charsNumber = str.toString().toCharArray();
+        for (int i = 0; i < charsNumber.length; i++) {
+            array[i] = Integer.parseInt(String.valueOf(charsNumber[i]));
         }
-        System.out.println(Arrays.toString(result));
+
         return array;
     }
 
     private static int sumArrayElements(String value) {
-        return 0;
+        int[] array = arrayInt(value);
+        int result = 0;
+        for (int j : array) {
+            result += j;
+        }
+        return result;
     }
 
 }
